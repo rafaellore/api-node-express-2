d2433fc3b3df28af4cdb26995f53bc61154389de
rafaellore <birdneon3@gmail.com>
2024-12-20 14:02:03 -0300

    feat(middlewares) improves errors messages

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -5,6 +5,14 @@ function manipuladorDeErros(erro, req, res, next) {
     res
       .status(400)
       .send({ message: "Um ou mais dados fornecidos estão incorretos." });
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(erro.errors)
+      .map((erro) => erro.message)
+      .join("; ");
+
+    res.status(400).send({
+      message: `Os seguintes erros foram encontrados: ${mensagensErro}`,
+    });
   } else {
     res.status(500).send({ message: "Erro interno de servidor." });
   }
