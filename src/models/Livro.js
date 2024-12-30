49bfcbb5162414f5310668ed6d185f77613d7ecb
rafaellore <birdneon3@gmail.com>
2024-12-30 13:41:44 -0300

    feat(Livro): adding better model validation and custom messages

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -14,8 +14,22 @@ const livroSchema = new mongoose.Schema({
   editora: {
     type: String,
     required: [true, "A editora é obrigatória"],
+    enum: {
+      values: ["Casa do código", "Alura"],
+      message: "A editora {VALUE} não é um valor permitido.",
+    },
   },
-  numeroPaginas: { type: Number },
+  numeroPaginas: {
+    type: Number,
+    min: [
+      10,
+      "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+    ],
+    max: [
+      5000,
+      "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}",
+    ],
+  },
 });
 
 const livros = mongoose.model("livros", livroSchema);
