dc484eb40e4c68a2b20dfa927d4babd53e3b20ed
rafaellore <birdneon3@gmail.com>
2025-01-27 21:23:46 -0300

    feat(livrosController): adds ordenation for list book method

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -5,14 +5,18 @@ import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 class LivroController {
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosResultado = await livros
           .find()
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
